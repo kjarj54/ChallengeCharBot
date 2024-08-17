fd2818b5b8eb007b9b9c777ca0f35dc9426eb9ff
Arauz <86898345+kjarj54@users.noreply.github.com>
2024-08-17 10:57:52 -0600

    [UPDATE] main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <Dashboard/>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="text-center p-8 bg-white rounded-lg shadow-lg animate-pulse">
+          <h1 className="text-4xl font-bold">Bienvenido</h1>
+        </div>
+      </div>
+      <Dashboard />
     </>
   );
 }
